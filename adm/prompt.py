61f5fc000a27657a852bdf9e43cb1ed703cfd745
brageon <153751096+brageon@users.noreply.github.com>
2023-12-16 00:07:55 +0100

    Update prompt.py

--- adm/prompt.py
+++ adm/prompt.py
@@ -1,10 +1,17 @@
 '''
-It says string or bytes expected in translate. I will review the logic someday. 
+Run socks.py inside another tab before this. Log.py is for old version. I have nlptools on PC.  
+
+nc -l 1234 works if socks.py will not. 
 '''
-import re, csv, nltk, time, scipy, numpy, socket
-import threading, logging, warnings, subprocess
-from nlptools.preprocessing.tagging import MLTagger
-from sklearn.linear_model import LinearRegression
+import re, nltk, logging, socket, threading, warnings
+import pandas as pd
+import numpy as np
+from nltk.tokenize import word_tokenize
+from nltk.tag import DefaultTagger
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.cluster import KMeans
 tag_map = {'NNP': 'Z', 'NNPS': 'Z', 'NN': 'Z', 'NNS': 'Z', 'PRP': 'A', 'WP': 'Z', 'WPS': 'Z', 'UH': 'S', 'DT': 'S', 'WDT': 'S', 'IN': 'J', 'CD': 'J', 'FW': 'J',
 'JJ': 'T', 'JJR': 'T', 'JJS': 'T', 'RB': 'T', 'RBR': 'T', 'RBS': 'T', 'VBP': 'G', 'VBZ': 'G', 'VBD': 'G', 'VBG': 'G', 'VBN': 'G', 'VB': 'G',
 'RP': 'A', 'EX': 'A', 'CC': 'A', 'WRB': 'A', 'MD': 'A', '.': 'n'}
@@ -16,8 +23,8 @@ logger = logging.getLogger()
 lock = threading.Lock()
 server = ('localhost', 1234)
 class LDA:
-    def __init__(self):
-        self.tagger = MLTagger()
+    def __init__(self, tags):
+        self.tagger = DefaultTagger(tags)
     def translate(self, text):
         tokens = nltk.word_tokenize(text)
         tag_pairs = nltk.pos_tag(tokens)
@@ -37,12 +44,11 @@ class LDA:
                 previous_word = word
             else:
                 word_list.append(str(word))
-        return word_list   
+        return word_list
     def amrita(self, words):
         prev_letter = None
         words = self.translate(words)
-        words = [str(word) for word in words]
-        words = ' '.join(words)
+        words = words.split(' ')
         line_sum = sum([float(dict_map.get(word, 0)) for word in words])
         count, special_cases = 0, 0
         line = ' '.join(words)
@@ -56,7 +62,7 @@ class LDA:
                 line_sum -= 7
                 special_cases += 1
             elif words[i - 1] == "T" and words[i] == "J":
-                line_sum -= 5 
+                line_sum -= 5
                 special_cases += 1
             elif words[i - 1] == "J" and words[i] == "T":
                 line_sum -= 9
@@ -100,22 +106,90 @@ class LDA:
             logger.info("Received data: %s" % data)
             if not data:
                 break
-def handle_input():
-    while True:
-        user1 = input("P1: ")
-        user2 = input("P2: ")
-        user3 = input("P3: ")    
-        if user1 == "stop":
-            return
-        else:
-            words = [user1, user2, user3]
-    return words
+    def process_log(data):
+        line, sums, means = [], [], []
+        for tup in data:
+            line.append(re.sub(r"[()\',]", "", tup[0]))
+            sums.append(re.sub(r"[()\',]", "", tup[1]))
+            means.append(re.sub(r"[()\',]", "", tup[2]))
+        line_arr = numpy.array(line)
+        line_df = pandas.DataFrame(line_arr, columns=['user'])
+        line_df.to_csv('line.csv', index=False)
+        sums_arr = numpy.array(sums, dtype=float)
+        means_arr = numpy.array(means, dtype=float)
+        chosen_values = []
+        for item, s, m in zip(line_arr, sums_arr, means_arr):
+            frequencies = {}
+        for char in item:
+            if char in frequencies:
+                frequencies[char] += 1
+            else:
+                frequencies[char] = 1
+        closest_mean = float('inf')
+        closest_value = ''
+        for char in item:
+            mean_difference = diff(dict_map[char], m)
+            if mean_difference < closest_mean:
+                closest_mean = mean_difference
+                closest_value = char
+        chosen_values.append(closest_value)
+        numu = len(chosen_values) // 2
+        names = [f'P{i + 1}' for i in range(numu)]
+        split_values = numpy.array(chosen_values).reshape(-1, numu)
+        df = pandas.DataFrame(split_values, columns=names)
+        df.to_csv('lisp.csv', index=False)
+        with open('lisp.csv', 'a') as f:
+            f.write(line + "," + sums + "," + ouro + "\n")
+    def log_handler():
+        while True:
+            with open("res.log", "r") as f:
+                for line in f:
+                    logging.info(line.strip())
+                t = threading.Thread(target=log_handler)
+                t.daemon = True  # exit with the main thread
+                t.start()
+    def generate_prompts(self, words):
+        tokenized_words = [word_tokenize(sentence) for sentence in words]
+        stemmer = WordNetLemmatizer()
+        stemmed_words = []
+        for sentence in tokenized_words:
+            stemmed_sentence = []
+            for word in sentence:
+                stemmed_sentence.append(stemmer.lemmatize(word))
+            stemmed_words.append(stemmed_sentence)
+            prompts = []
+        for i in range(len(stemmed_words)):
+            prompt = ""
+        for word in stemmed_words[i]:
+            prompt += word + " "
+        prompts.append(prompt)
+        return prompts
+    def log_output(self, output):
+        logging.info(output)
+        with open('output.log', 'a') as f:
+            f.write(output + '\n')
+    def handle_input(self):
+        while True:
+            user1 = input("P1: ")
+            user2 = input("P2: ")
+            user3 = input("P3: ")
+            if user1 == "stop":
+                return
+            else:
+                self.log_output(f"P1: {user1}; P2: {user2}; P3: {user3}")
+                words = [user1, user2, user3]
+                prompts = self.generate_prompts(words)
+                for prompt in prompts:
+                    print(prompt)
 def main():
-    words = handle_input()
-    lda = LDA()
-    sock = lda.connect_to_server()
     while True:
-        line, sums, ouro = lda.amrita(words)
-        lda.listen_from_server(sock)
+        lda = LDA(tag_map)
+        words = lda.handle_input()
+        sock = lda.connect_to_server()
+        while True:
+            line, sums, ouro = lda.amrita(words)
+            lda.listen_from_server(sock)
+            lda.log_output(f"line: {line}; sums: {sums}; ouro: {ouiro}")
+            # process_log(f"line: {line}; sums: {sums}; ouro: {ouiro}")
 if __name__ == '__main__':
     main()
