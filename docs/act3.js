876f577a023841de4b2ccef5f7606893a3def75e
Daniel Jonsson <153751096+brageon@users.noreply.github.com>
2024-05-09 21:32:22 +0200

    Update act3.js

--- docs/act3.js
+++ docs/act3.js
@@ -1,9 +1,9 @@
-const mySecret = process.env.DULL
+// Nodejs & Ajax is required for process.env.DULL; 
 const promptButton = document.getElementById('promptButton');
 promptButton.addEventListener('click', async () => {
   const userInput = await prompt('Patience is virtue:');
   if (!userInput) { return; }
-  try { const response = await fetch(mySecret, {
+  try { const response = await fetch(/, {
     method: 'POST', body: userInput,
     headers: { 'Content-Type': 'text/plain' }});
     window.open('https://ddarwin.s3.eu-north-1.amazonaws.com/dove.txt', '_blank');
