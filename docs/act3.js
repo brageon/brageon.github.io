456228614978c2689fef1c0e2a9ee0f92df9e458
Daniel Jonsson <153751096+brageon@users.noreply.github.com>
2024-05-09 20:07:04 +0200

    Window

--- docs/act3.js
+++ docs/act3.js
@@ -1,6 +1,6 @@
 const promptButton = document.getElementById('promptButton');
 promptButton.addEventListener('click', async () => {
-  const userInput = await prompt('Enter your message:');
+  const userInput = await prompt('Reload S3 after 15 seconds:');
   if (!userInput) { return; }
   try { const response = await fetch('https://o66gcrisu76svuos65hnrembza0myxeg.lambda-url.eu-north-1.on.aws/', {
     method: 'POST', body: userInput,
@@ -8,6 +8,5 @@ promptButton.addEventListener('click', async () => {
     window.open('https://ddarwin.s3.eu-north-1.amazonaws.com/dove.txt', '_blank');
     if (!response.ok) {
       throw new Error(`Error triggering workflow: ${response.statusText}`); }
-    setTimeout(() => { window.location.reload(); }, 15000); 
   } catch (error) { console.error('Error:', error);
     alert('An error occurred. Please try again.'); }});
